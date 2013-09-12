62206264f03a97389c5b9dc0d70fabeb94d0361d
Mahdi Haji <m8942001@yahoo.com>
2013-09-11 23:13:19 -0700

    Initial Commit

---
+++ SD_code_project/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SD_code_project
+//
+//  Created by mahdi hajiaghayi on 9/11/13.
+//  Copyright (c) 2013 mahdi hajiaghayi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
